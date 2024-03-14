eca412bb968f76382a1a1701f5c9389ffa75ce5c
tangwwwei <20863759+tangwwwei@users.noreply.github.com>
2024-03-14 20:18:55 +0800

    Deploy after running pnpm build and changing vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import liveReload from 'vite-plugin-live-reload';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flag-guessing-game-frontend/',
+  base: '/',
   plugins: [
     liveReload('**/*'),
     react(),
